f9af5ad22531489bd79c14f7b1116e3f3db54b1c
Tito Rizqy Putra Wiyana <titorpw123@gmail.com>
2025-05-20 20:22:12 +0700

    Coba Branch (2)

--- app/src/main/java/com/l0123137/to_duh/MainActivity.kt
+++ app/src/main/java/com/l0123137/to_duh/MainActivity.kt
@@ -21,7 +21,6 @@ class MainActivity : ComponentActivity() {
             ToDuhTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "halo",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,7 +30,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(modifier: Modifier = Modifier) {
     Text(
         text = "Hello To-Duh!, Coba-Coba",
         modifier = modifier
@@ -42,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     ToDuhTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
